22d2d9437efb72db508dda605aab3a4b102bfd1c
KomarovaNastasya <komarova.k.nastasya@gmail.com>
2022-03-10 01:57:34 +0300

    Некоторые изменения

--- main.py
+++ main.py
@@ -2,16 +2,18 @@
 
 import telebot  # pyTelegramBotAPI	4.3.1
 
-bot = telebot.TeleBot('5250359342:AAE4qvngPu5RiSbjFsl1Z_pTULiA-qW5puc')  # Создаем экземпляр бота @Ivanov_Ivan_1MD19_bot
+bot = telebot.TeleBot('5250359342:AAE4qvngPu5RiSbjFsl1Z_pTULiA-qW5puc')
 
 # -----------------------------------------------------------------------
 # Функция, обрабатывающая команду /start
-@bot.message_handler(commands=["start"])
+
+
+@bot.message_handler(commands=['start'])
 def start(message, res=False):
     chat_id = message.chat.id
 
     bot.send_message(chat_id,
-                     text="Привет, {0.first_name}! Я тестовый бот для курса программирования на языке ПаЙтон".format(
+                     text="Привет, {0.first_name}! Я тестовый бот для курса программирования на языке Пайтон".format(
                          message.from_user))
 
 
@@ -21,10 +23,11 @@ def start(message, res=False):
 def get_text_messages(message):
     chat_id = message.chat.id
     ms_text = message.text
-    bot.send_message(chat_id, text="Я тебя слышу!!! Ваше сообщение: " + ms_text)
+    bot.send_message(chat_id, text="Ваше сообщение: " + ms_text)
 
 
 # -----------------------------------------------------------------------
-bot.polling(none_stop=True, interval=0) # Запускаем бота
+# Запускаем бота
+bot.polling(none_stop=True, interval=0)
 
 print()
